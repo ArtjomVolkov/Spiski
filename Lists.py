446bae5c4cb5831b80345bf933c2bd613a4299cf
ArtjomVolkov <93324363+ArtjomVolkov@users.noreply.github.com>
2021-12-03 10:03:19 +0200

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -66,15 +66,67 @@
 #sps.remove(max)
 #sps.insert(ind,new_max)
 #print(sps)
-from random import*
-sps=[]
-for i in sps:
-	sps.append(abs(i))
-print(sps.sort())
-print(sps.sort(reverse=True))
+#from random import*
+#sps=[]
+#for i in sps:
+#	sps.append(abs(i))
+#print(sps.sort())
+#print(sps.sort(reverse=True))
+#print()
+#l=['крот', 'белка', 'выхухоль']
+#nl=[]
+#max=0
+#for e in l:
+#	x=len(e)
+#	if x>max:
+#		max=x
+#for e in l:
+#	nl.append(e.ljust(max,"_"))
+#print(nl)
+#s1=['крот', 'белка', 'выхухоль']
+#s2=['a', 'aa', 'aaa', 'aaaa', 'aaaaa']
+#s3=['qweasdqweas', 'q', 'rteww', 'ewqqqqq']
+#ss=[s1,s2,s3]
+#N=0
+#while N<len(ss):
+#	print(ss[N])
+#	max=0
+#	Sn_=[]
+#	for s in ss[N]:
+#		pikkus=len(s)
+#		if pikkus>max: max=pikkus
+#	for s in ss[N]:
+#		Sn_.append(s.ljust(max,"_"))
+#	print(Sn_)
+#	N+=1
+glas="aeiou"
+cogl="bcdfghjklmpqrstwxyz"
+a=[]
+b=[]
+abc=[]
+while 1:
+	try:
+		nimi=str(input("Sisestage oma nimi =>"))
+		break
+	except ValueError:
+		print("Ainult kirju!")
 print()
-a=['крот', 'белка', 'выхухоль']
-['a', 'aa', 'aaa', 'aaaa', 'aaaaa']
-['qweasdqweas', 'q', 'rteww', 'ewqqqqq']
-a.rjust(5,"_")
-print(a)
+print("Welcome!",nimi.title())
+print()
+#nimi.isalpha()
+#nimi.split(" ")
+for i in nimi:
+	if i in glas:
+		a.append(i)
+print("Vokaalid => ",len(a))
+for e in nimi:
+	if e in cogl:
+		b.append(e)
+print("Kaashäälikud => ",len(b))
+print()
+abc=list(nimi.lower())
+uksikud=[x for x in abc if abc.count(x)>=1]
+uksikud=list(set(uksikud))
+print(uksikud)
+n=["A"," E"," I"," O", "U"]
+l=["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S"," T", "V", "W", "X"," Y", "Z"]
